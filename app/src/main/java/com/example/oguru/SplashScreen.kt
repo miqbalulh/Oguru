0d2752a2c87682c04e16d9ac4445f2980f99be34
miqbalulh <iqbalul.hidayat2801@gmail.com>
2020-02-11 16:33:20 +0700

    Splash Screen

---
+++ app/src/main/java/com/example/oguru/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.example.oguru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.oguru.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
